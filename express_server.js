b9d51b54dd2d7ffcb0eac9d3c4137d72484f90f9
Lucas <cekanlucas@gmail.com>
2020-02-17 22:14:29 +0000

    added app get for /urls

--- express_server.js
+++ express_server.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
